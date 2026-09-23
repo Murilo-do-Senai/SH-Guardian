e99274e4114dd5915b075708e25c7c53965f8a83
Murilo-do-Senai <murilo.m.almeida6@aluno.senai.br>
2026-09-23 08:11:24 -0300

    upd1.1

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import ClienteRoute from './routes/ClientesRoute.js';
 
 const app = express();
 app.use(express.json());
@@ -7,6 +8,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.json({ ok: true, api: "SH Guardian API" }));
+
+app.use("/clientes",ClienteRoute);
 
 
 export default app;
