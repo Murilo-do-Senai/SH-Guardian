5ec4a70f053c4dfd25abc1262be910a91c2e7272
Murilo-do-Senai <murilo.m.almeida6@aluno.senai.br>
2026-09-23 16:00:18 -0300

    upd1.2.2

--- backend/src/routes/ClientesRoute.js
+++ backend/src/routes/ClientesRoute.js
@@ -5,8 +5,12 @@ const router = Router();
 
 router.get("/",ClientesController.ListarCliente);
 router.get("/:id",ClientesController.ListarClienteId);
-router.post("/",ClientesController.RegistroCliente);
-router.put("/:id",ClientesController.AtualizarCliente);
-router.delete("/:id",ClientesController.DeletarCliente);
+router.post("/reg",ClientesController.RegistroCliente);
+router.put("/up-:id",ClientesController.AtualizarCliente);
+router.delete("/del-:id",ClientesController.DeletarCliente);
+
+router.post("/ad_fone:id",ClientesController.RegistroTelefone)
+router.put("/up_fone:id",ClientesController.AtualizarTelefone)
+router.delete("/del_fone:id",ClientesController.DeletarTelefone)
 
 export default router;
