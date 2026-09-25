d7973f7c151e2476154eddcce6951517d0a46c8f
Murilo-do-Senai <murilo.m.almeida6@aluno.senai.br>
2026-09-25 14:04:33 -0300

    upd1.2.3

--- backend/src/routes/ClientesRoute.js
+++ backend/src/routes/ClientesRoute.js
@@ -5,12 +5,16 @@ const router = Router();
 
 router.get("/",ClientesController.ListarCliente);
 router.get("/:id",ClientesController.ListarClienteId);
-router.post("/reg",ClientesController.RegistroCliente);
-router.put("/up-:id",ClientesController.AtualizarCliente);
-router.delete("/del-:id",ClientesController.DeletarCliente);
-
-router.post("/ad_fone:id",ClientesController.RegistroTelefone)
-router.put("/up_fone:id",ClientesController.AtualizarTelefone)
-router.delete("/del_fone:id",ClientesController.DeletarTelefone)
+router.post("/add",ClientesController.RegistroCliente);
+router.put("/up/:id",ClientesController.AtualizarCliente);
+router.delete("/del/:id",ClientesController.DeletarCliente);
+/////////////////////////////////////////////////////////////////////////////////////////////
+router.post("/ad_fone/:id",ClientesController.RegistroTelefone)
+router.put("/up_fone/:id_cliente/:id_telefone",ClientesController.AtualizarTelefone)
+router.delete("/del_fone/:id_cliente/:id_telefone",ClientesController.DeletarTelefone)
+/////////////////////////////////////////////////////////////////////////////////////////////
+router.post("/ad_car/:id",ClientesController.RegistroCartao)
+router.put("/up_car/:id_cliente/:id_cartao",ClientesController.AtualizarCartao)
+router.delete("/del_car/:id_cliente/:id_cartao",ClientesController.DeletarCartao)
 
 export default router;
